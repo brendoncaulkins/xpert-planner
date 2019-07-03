74ae4032d626b1ffcc393e9e1e1bfa65d315d6ed
brendon.caulkins <brendon.caulkins@gmail.com>
2019-07-02 20:22:41 -0400

    Initial linting pass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'xpert-plan-sandbox';
+  title = 'xpert-plan-sandbox'
 }
