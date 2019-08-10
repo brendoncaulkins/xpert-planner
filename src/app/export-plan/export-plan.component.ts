b1ae040d734f49d660b28032f94a739cef57ef4e
brendoncaulkins <brendon.caulkins@excella.com>
2019-08-10 09:21:57 -0600

    Basic Export functionality

--- src/app/export-plan/export-plan.component.ts
+++ src/app/export-plan/export-plan.component.ts
@@ -1,12 +1,51 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core'
+import { FormControl, Validators } from '@angular/forms'
+import { SubSink } from 'subsink'
+
+import { IPlanItem } from '../models/xpert-plan.interface'
+import { PlanService } from '../services/plan/plan.service'
+import { ISupportedFileType, supportedFileTypes } from '../utils/file-conversions'
 
 @Component({
   selector: 'app-export-plan',
   templateUrl: './export-plan.component.html',
   styleUrls: ['./export-plan.component.css'],
 })
-export class ExportPlanComponent implements OnInit {
-  constructor() {}
+export class ExportPlanComponent implements OnDestroy {
+  exportOptions = supportedFileTypes
 
-  ngOnInit() {}
+  exportFormat = new FormControl(null, Validators.required)
+
+  subs = new SubSink()
+
+  constructor(private planService: PlanService) {}
+
+  ngOnDestroy() {
+    this.subs.unsubscribe()
+  }
+
+  onExport() {
+    const format: ISupportedFileType = this.exportFormat.value
+    this.subs.add(
+      this.planService.plan$.subscribe((plan: IPlanItem[]) =>
+        this.offerFile(format.exportFn(plan), format.extension)
+      )
+    )
+  }
+
+  offerFile(data: string, format: string) {
+    const blob = new Blob([data], { type: `text/${format}` })
+    const filename = `xpert_plan.${format}`
+
+    if (window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveBlob(blob, filename)
+    } else {
+      const elem = window.document.createElement('a')
+      elem.href = window.URL.createObjectURL(blob)
+      elem.download = filename
+      document.body.appendChild(elem)
+      elem.click()
+      document.body.removeChild(elem)
+    }
+  }
 }
---
+++ src/app/utils/file-conversions.ts
@@ -0,0 +1,63 @@
+import { IPlanItem } from '../models/xpert-plan.interface'
+
+export enum FileType {
+  CSV,
+  JSON,
+}
+
+export interface ISupportedFileType {
+  fileType: FileType
+  description: string
+  extension: string
+  exportFn: (items: IPlanItem[]) => string
+  importFn: (data: any) => IPlanItem[]
+}
+
+export const supportedFileTypes: ISupportedFileType[] = [
+  {
+    fileType: FileType.CSV,
+    description: 'Comma Separated Value',
+    extension: 'csv',
+    exportFn: exportAsCsv,
+    importFn: importFromCsv,
+  },
+  {
+    fileType: FileType.JSON,
+    description: 'JavaScript Object Notation',
+    extension: 'json',
+    exportFn: exportAsJson,
+    importFn: importFromJson,
+  },
+]
+
+export function exportAsCsv(items: IPlanItem[]): string {
+  const header = 'Category,Description,Points,Completed,Completed On'
+  const rows = items.map(
+    i =>
+      `${i.baseItem.category.name},${i.description},${i.points},${
+        i.completed
+      },${i.coompletedOn.toLocaleDateString()}`
+  )
+  rows.unshift(header)
+  return rows.join('\n')
+}
+
+export function importFromCsv(data: any): IPlanItem[] {
+  return []
+}
+
+export function exportAsJson(items: IPlanItem[]): string {
+  return JSON.stringify(
+    items.map(i => ({
+      category: i.baseItem.category.name,
+      description: i.description,
+      points: i.points,
+      completed: i.completed,
+      completedOn: i.coompletedOn,
+    }))
+  )
+}
+
+export function importFromJson(data: any): IPlanItem[] {
+  return []
+}
