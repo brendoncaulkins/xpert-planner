04a8ff5a4dc0146be6dfeadceb691dc189be7ce8
brendoncaulkins <brendon.caulkins@excella.com>
2019-08-10 13:49:10 -0400

    Install ng2-charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core'
 import { FlexLayoutModule } from '@angular/flex-layout'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { ChartsModule } from 'ng2-charts'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
@@ -12,13 +13,13 @@ import { ExportPlanComponent } from './export-plan/export-plan.component'
 import { HomeComponent } from './home/home.component'
 import { ImportPlanComponent } from './import-plan/import-plan.component'
 import { MaterialModule } from './material.module'
+import { OverviewComponent } from './overview/overview.component'
 import { ItemFormComponent } from './plan/item-form/item-form.component'
 import { ItemListComponent } from './plan/item-list/item-list.component'
 import { PlanComponent } from './plan/plan.component'
 import { BaseItemService } from './services/base-item/base-item.service'
 import { CategoryService } from './services/category/category.service'
-import { PlanService } from './services/plan/plan.service';
-import { OverviewComponent } from './overview/overview.component'
+import { PlanService } from './services/plan/plan.service'
 
 @NgModule({
   declarations: [
@@ -34,6 +35,7 @@ import { OverviewComponent } from './overview/overview.component'
     OverviewComponent,
   ],
   imports: [
+    ChartsModule,
     CommonModule,
     BrowserAnimationsModule,
     MaterialModule,
