a204397143c10d60ff4360eb8617beaf62c47c3e
brendoncaulkins <brendon.caulkins@excella.com>
2019-08-09 19:28:13 -0600

    Export Plan controls & linting

--- src/app/export-plan/export-plan.component.ts
+++ src/app/export-plan/export-plan.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
 
 @Component({
   selector: 'app-export-plan',
   templateUrl: './export-plan.component.html',
-  styleUrls: ['./export-plan.component.css']
+  styleUrls: ['./export-plan.component.css'],
 })
 export class ExportPlanComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
