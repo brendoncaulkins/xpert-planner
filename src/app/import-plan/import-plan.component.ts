0161ae5332217f0a2ee1215071307faf4861442a
brendoncaulkins <brendon.caulkins@excella.com>
2019-08-09 19:56:27 -0600

    Basic Plan Import controls

--- src/app/import-plan/import-plan.component.ts
+++ src/app/import-plan/import-plan.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-import-plan',
   templateUrl: './import-plan.component.html',
-  styleUrls: ['./import-plan.component.css']
+  styleUrls: ['./import-plan.component.css'],
 })
-export class ImportPlanComponent implements OnInit {
+export class ImportPlanComponent {
+  constructor() {}
 
-  constructor() { }
+  onFileSelected() {
+    const inputNode: any = document.querySelector('#file-upload')
 
-  ngOnInit() {
+    if (typeof FileReader !== 'undefined') {
+      const reader = new FileReader()
+
+      reader.onload = (e: any) => {
+        console.log(e)
+      }
+
+      reader.readAsArrayBuffer(inputNode.files[0])
+    }
   }
-
 }
