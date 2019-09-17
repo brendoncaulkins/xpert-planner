1270d943b627144f8e44d245fa40033d6493de00
brendoncaulkins <brendon.caulkins@excella.com>
2019-09-17 00:20:50 -0400

    Remove useless intermediary component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,11 +13,10 @@ import { HomeComponent } from './home/home.component'
 import { ImportPlanComponent } from './import-plan/import-plan.component'
 import { MaterialModule } from './material.module'
 import { OverviewComponent } from './overview/overview.component'
+import { CompletedTableComponent } from './plan/completed-table/completed-table.component'
+import { ForecastedTableComponent } from './plan/forecasted-table/forecasted-table.component'
 import { ItemFormComponent } from './plan/item-form/item-form.component'
 import { ItemListComponent } from './plan/item-list/item-list.component'
-import { CompletedTableComponent } from './plan/plan-display/completed-table/completed-table.component'
-import { ForecastedTableComponent } from './plan/plan-display/forecasted-table/forecasted-table.component'
-import { PlanDisplayComponent } from './plan/plan-display/plan-display.component'
 import { PlanComponent } from './plan/plan.component'
 import { BaseItemService } from './services/base-item/base-item.service'
 import { CategoryService } from './services/category/category.service'
@@ -34,7 +33,6 @@ import { PlanService } from './services/plan/plan.service'
     ImportPlanComponent,
     ExportPlanComponent,
     OverviewComponent,
-    PlanDisplayComponent,
     CompletedTableComponent,
     ForecastedTableComponent,
   ],
--- src/app/plan/plan.component.spec.ts
+++ src/app/plan/plan.component.spec.ts
@@ -7,11 +7,10 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 import { MaterialModule } from '../material.module'
 import { CategoryService } from '../services/category/category.service'
 import { PlanService } from '../services/plan/plan.service'
+import { CompletedTableComponent } from './completed-table/completed-table.component'
+import { ForecastedTableComponent } from './forecasted-table/forecasted-table.component'
 import { ItemFormComponent } from './item-form/item-form.component'
 import { ItemListComponent } from './item-list/item-list.component'
-import { CompletedTableComponent } from './plan-display/completed-table/completed-table.component'
-import { ForecastedTableComponent } from './plan-display/forecasted-table/forecasted-table.component'
-import { PlanDisplayComponent } from './plan-display/plan-display.component'
 import { PlanComponent } from './plan.component'
 
 describe('PlanComponent', () => {
@@ -29,7 +28,6 @@ describe('PlanComponent', () => {
       ],
       declarations: [
         PlanComponent,
-        PlanDisplayComponent,
         ItemFormComponent,
         ItemListComponent,
         CompletedTableComponent,
