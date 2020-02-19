a925a8c776a6c230a82c814a5484ded56b0fbee8
brendoncaulkins <brendon.caulkins@excella.com>
2020-02-18 23:50:27 -0500

    UnSavedDataGuard coverage

--- src/app/guards/unsaved-data/unsaved-data.guard.spec.ts
+++ src/app/guards/unsaved-data/unsaved-data.guard.spec.ts
@@ -1,45 +1,75 @@
 import { TestBed, inject } from '@angular/core/testing'
 import { MatDialogModule } from '@angular/material'
-import { DialogService } from 'src/app/messaging/services/dialog/dialog.service'
-import { IPlanItem } from 'src/app/models/xpert-plan.interface'
-import { PlanService } from 'src/app/services/plan/plan.service'
+import { of } from 'rxjs'
+import { PlanComponent } from 'src/app/plan/plan.component'
 
+import { DialogService } from '../../messaging/services/dialog/dialog.service'
+import { plan } from '../../models/mock.data'
+import { IPlanItem } from '../../models/xpert-plan.interface'
+import { PlanService } from '../../services/plan/plan.service'
 import { UnsavedDataGuard } from './unsaved-data.guard'
 
 describe('UnsavedDataGuard', () => {
   let dialogService: DialogService
-  let planService: PlanService
+  const planService = jasmine.createSpyObj('PlanService', ['plan$'])
 
   beforeEach(() => {
+    planService.plan$ = of(plan)
     TestBed.configureTestingModule({
       imports: [MatDialogModule],
-      providers: [UnsavedDataGuard, DialogService, PlanService],
+      providers: [
+        UnsavedDataGuard,
+        DialogService,
+        { provide: PlanService, useValue: planService },
+      ],
     })
     dialogService = TestBed.get(DialogService)
-    planService = TestBed.get(PlanService)
   })
 
-  it('should implement CanDeactivate', inject(
-    [UnsavedDataGuard],
-    (guard: UnsavedDataGuard) => {
+  describe('canDeactivate', () => {
+    let guard: UnsavedDataGuard
+    let planComponent: PlanComponent
+
+    beforeEach(() => {
+      planComponent = jasmine.createSpyObj('PlanComponent', ['getFormPlan'])
+      guard = new UnsavedDataGuard(dialogService, planService)
+    })
+
+    it('should implement correctly', () => {
       expect(guard).toBeTruthy()
       expect(guard.canDeactivate).toBeDefined()
-    }
-  ))
+    })
+    it('should return true when there is no unsaved data', done => {
+      planComponent.getFormPlan = jasmine.createSpy('getFormPlan').and.returnValue(plan)
+
+      spyOn(guard, 'hasUnsavedData').and.callThrough()
+      guard.canDeactivate(planComponent).subscribe(result => {
+        expect(guard.hasUnsavedData).toHaveBeenCalled()
+        expect(result).toBe(true)
+        done()
+      })
+    })
+    it('should ask for confirmation when there is unsaved data', done => {
+      planComponent.getFormPlan = jasmine
+        .createSpy('getFormPlan')
+        .and.returnValue(plan.slice(1))
+
+      spyOn(guard, 'doConfirm').and.returnValue(of(true))
+      guard.canDeactivate(planComponent).subscribe(result => {
+        expect(guard.doConfirm).toHaveBeenCalled()
+        expect(result).toBe(true)
+        done()
+      })
+    })
+  })
 
   describe('hasUnsavedData', () => {
-    let plan: IPlanItem[]
     let formData: IPlanItem[]
     let guard: UnsavedDataGuard
 
     beforeEach(() => {
       guard = new UnsavedDataGuard(dialogService, planService)
-      const planItem = {
-        description: 'A description',
-        baseItem: { type: 'Base Item', points: 5 },
-      } as IPlanItem
-      plan = [{ ...planItem }]
-      formData = [{ ...planItem }]
+      formData = plan.map(i => ({ ...i }))
     })
 
     it('should return false when formData is an empty array', () => {
@@ -55,6 +85,22 @@ describe('UnsavedDataGuard', () => {
     })
     it('should return false when the plan and formData have the same data', () => {
       expect(guard.hasUnsavedData(plan, formData)).toBe(false)
+    })
+  })
+
+  describe('doConfirm', () => {
+    let guard: UnsavedDataGuard
+    let planComponent: PlanComponent
+
+    beforeEach(() => {
+      planComponent = jasmine.createSpyObj('PlanComponent', ['getFormPlan', 'onSave'])
+      planComponent.onSave = jasmine.createSpy('onSave')
+      guard = new UnsavedDataGuard(dialogService, planService)
+    })
+    it('should call the dialogService', () => {
+      spyOn(dialogService, 'confirm')
+      guard.doConfirm(planComponent)
+      expect(dialogService.confirm).toHaveBeenCalled()
     })
   })
 })
