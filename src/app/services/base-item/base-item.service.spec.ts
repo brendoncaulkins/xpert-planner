df27e68cef478703962e8080c0ff9c7879e8ef01
brendoncaulkins <brendon.caulkins@excella.com>
2020-02-19 00:41:01 -0500

    Stop messing so much with mock data

--- src/app/services/base-item/base-item.service.spec.ts
+++ src/app/services/base-item/base-item.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing'
 import { of } from 'rxjs'
 
 import { baseItems, categories } from '../../models/mock.data'
+import { IBasePlanItem } from '../../models/xpert-plan.interface'
 import { CategoryService } from '../category/category.service'
 import { BaseItemService } from './base-item.service'
 
@@ -26,8 +27,24 @@ describe('BaseItemService', () => {
   })
 
   describe('resolve()', () => {
+    const dehydratedBaseItems: IBasePlanItem[] = [
+      {
+        categoryId: 1,
+        points: 1,
+        type: 'Sample Item',
+      } as IBasePlanItem,
+      {
+        categoryId: 2,
+        points: 5,
+        type: 'Other Sample Item',
+      } as IBasePlanItem,
+      {
+        categoryId: 2,
+        points: 3,
+        type: 'A third Item',
+      } as IBasePlanItem,
+    ]
     it('should provide hydrated baseItems', done => {
-      const dehydratedBaseItems = baseItems.map(i => ({ ...i, category: null }))
       spyOn(service, 'fetch').and.returnValue(of(dehydratedBaseItems))
 
       service.resolve().subscribe(result => {
