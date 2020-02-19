54b330d5f3e48f4aff3b6fbfe67b98308038cd2c
brendoncaulkins <brendon.caulkins@excella.com>
2020-02-18 23:05:56 -0500

    BaseItemService custom resolver coverage

--- src/app/services/base-item/base-item.service.spec.ts
+++ src/app/services/base-item/base-item.service.spec.ts
@@ -1,19 +1,39 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { TestBed } from '@angular/core/testing'
+import { of } from 'rxjs'
 
+import { baseItems, categories } from '../../models/mock.data'
 import { CategoryService } from '../category/category.service'
 import { BaseItemService } from './base-item.service'
 
 describe('BaseItemService', () => {
-  beforeEach(() =>
+  let service: BaseItemService
+  const categoryService = jasmine.createSpyObj('CategoryService', ['fetch'])
+  categoryService.fetch = jasmine.createSpy('fetch').and.returnValue(of(categories))
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BaseItemService, CategoryService],
+      providers: [
+        BaseItemService,
+        { provide: CategoryService, useValue: categoryService },
+      ],
       imports: [HttpClientTestingModule],
     })
-  )
+    service = TestBed.get(BaseItemService)
+  })
 
   it('should be created', () => {
-    const service: BaseItemService = TestBed.get(BaseItemService)
     expect(service).toBeTruthy()
+  })
+
+  describe('resolve()', () => {
+    it('should provide hydrated baseItems', done => {
+      const dehydratedBaseItems = baseItems.map(i => ({ ...i, category: null }))
+      spyOn(service, 'fetch').and.returnValue(of(dehydratedBaseItems))
+
+      service.resolve().subscribe(result => {
+        expect(result).toEqual(baseItems)
+        done()
+      })
+    })
   })
 })
