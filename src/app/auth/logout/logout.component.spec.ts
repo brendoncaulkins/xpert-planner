ece92c5e7b889a0c05fe82ace586b67efcca9001
brendon.caulkins <brendon.caulkins@gmail.com>
2020-03-25 18:02:30 -0400

    Updated Logout tests

--- src/app/auth/logout/logout.component.spec.ts
+++ src/app/auth/logout/logout.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
+import { Router } from '@angular/router'
+import { RouterTestingModule } from '@angular/router/testing'
+import { ErrorComponent } from 'src/app/error/error.component'
 
-import { AuthService } from '../services/auth.service'
+import { AuthService, IAuthStatus } from '../services/auth.service'
 import { AuthServiceFake } from '../services/auth.service.fake'
 import { LogoutComponent } from './logout.component'
 
@@ -8,9 +11,13 @@ describe('LogoutComponent', () => {
   let component: LogoutComponent
   let fixture: ComponentFixture<LogoutComponent>
   let authService: AuthService
+  let router: Router
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'login', component: ErrorComponent }]),
+      ],
       declarations: [LogoutComponent],
       providers: [{ provide: AuthService, useClass: AuthServiceFake }],
     }).compileComponents()
@@ -21,6 +28,7 @@ describe('LogoutComponent', () => {
     component = fixture.componentInstance
     authService = TestBed.inject(AuthService)
     spyOn(authService, 'logout')
+    router = TestBed.inject(Router)
     fixture.detectChanges()
   })
 
@@ -30,5 +38,11 @@ describe('LogoutComponent', () => {
 
   it('should call authService.logout()', () => {
     expect(authService.logout).toHaveBeenCalled()
+  })
+
+  it('should redirect to the login page on successful logout', () => {
+    spyOn(router, 'navigate')
+    authService.authStatus$.next({ isAuthenticated: false } as IAuthStatus)
+    expect(router.navigate).toHaveBeenCalledWith(['/', 'login'])
   })
 })
