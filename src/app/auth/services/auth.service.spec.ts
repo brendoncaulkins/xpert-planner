37ee11eee78476cc16f45e575b9042acfa4ae79b
brendon.caulkins <brendon.caulkins@gmail.com>
2020-03-25 19:16:53 -0400

    Auth Service Tests

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -5,14 +5,16 @@ import { MessagingModule } from 'src/app/messaging/messaging.module'
 import { AuthService } from './auth.service'
 
 describe('AuthService', () => {
+  let service: AuthService
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, MessagingModule],
       providers: [AuthService],
     })
+    service = TestBed.inject(AuthService)
   })
 
-  it('should be created', inject([AuthService], (service: AuthService) => {
+  it('should be created', () => {
     expect(service).toBeTruthy()
-  }))
+  })
 })
