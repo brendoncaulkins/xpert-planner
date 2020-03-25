3a400e6107e3afb81d4e3b1dad7a60591fa77c5d
brendon.caulkins <brendon.caulkins@gmail.com>
2020-03-25 19:34:36 -0400

    Final Login Component

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,29 +1,65 @@
 import { ComponentFixture, TestBed, async } from '@angular/core/testing'
-import { MaterialModule } from 'src/app/material.module'
+import { ReactiveFormsModule } from '@angular/forms'
+import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+import { Router } from '@angular/router'
+import { RouterTestingModule } from '@angular/router/testing'
 
-import { AuthService } from '../services/auth.service'
+import { ErrorComponent } from '../../error/error.component'
+import { MaterialModule } from '../../material.module'
+import { AuthService, IAuthStatus } from '../services/auth.service'
 import { AuthServiceFake } from '../services/auth.service.fake'
 import { LoginComponent } from './login.component'
 
 describe('LoginComponent', () => {
   let component: LoginComponent
   let fixture: ComponentFixture<LoginComponent>
+  let authService: AuthService
+  let router: Router
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [MaterialModule],
+      imports: [
+        MaterialModule,
+        RouterTestingModule.withRoutes([{ path: 'planner', component: ErrorComponent }]),
+        ReactiveFormsModule,
+        NoopAnimationsModule,
+      ],
       declarations: [LoginComponent],
-      providers: [{ provide: AuthService, useCLass: AuthServiceFake }],
+      providers: [{ provide: AuthService, useClass: AuthServiceFake }],
     }).compileComponents()
   }))
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent)
     component = fixture.componentInstance
+    authService = TestBed.inject(AuthService)
+    router = TestBed.inject(Router)
     fixture.detectChanges()
   })
 
   it('should create', () => {
     expect(component).toBeTruthy()
+  })
+
+  it('should navigate when authService.authStatus$.isAuthenticated goes true', () => {
+    spyOn(router, 'navigate')
+    authService.authStatus$.next({ isAuthenticated: true } as IAuthStatus)
+    expect(router.navigate).toHaveBeenCalledWith(['/', 'planner'])
+  })
+
+  describe('login()', () => {
+    it('should call authService.login', () => {
+      spyOn(authService, 'login')
+      component.login()
+      expect(authService.login).toHaveBeenCalled()
+    })
+
+    it('should call login() with the appropriate inputs', () => {
+      spyOn(authService, 'login')
+      component.formGroup.setValue({ email: 'test@test.com', password: 'P@ssword!!' })
+      component.login()
+      expect(authService.login).toHaveBeenCalled()
+      expect(authService.login).toHaveBeenCalledWith('test@test.com', 'P@ssword!!')
+    })
   })
 })
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core'
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
-import { tap } from 'rxjs/operators'
 import { SubSink } from 'subsink'
 
 import { AuthService } from '../services/auth.service'
@@ -8,18 +8,36 @@ import { AuthService } from '../services/auth.service'
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
+  styles: [
+    `
+      .login-container {
+        width: 50%;
+        min-width: 200px;
+      }
+    `,
+  ],
 })
 export class LoginComponent implements OnInit, OnDestroy {
   subs = new SubSink()
+  formGroup: FormGroup
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
     this.subs.add(
-      this.authService.authStatus$.pipe(tap(console.log)).subscribe(status => {
+      this.authService.authStatus$.subscribe(status => {
         if (status.isAuthenticated) {
           this.router.navigate(['/', 'planner'])
         }
       })
     )
+
+    this.formGroup = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    })
   }
 
   ngOnInit(): void {}
@@ -29,6 +47,13 @@ export class LoginComponent implements OnInit, OnDestroy {
   }
 
   login() {
-    this.authService.login('brendon.caulkins@gmail.com', 'a test password!')
+    this.authService.login(this.email.value, this.password.value)
+  }
+
+  get email(): AbstractControl {
+    return this.formGroup.get('email')
+  }
+  get password(): AbstractControl {
+    return this.formGroup.get('password')
   }
 }
