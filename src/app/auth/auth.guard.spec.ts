930f73e04d091d438902de3abbee4c20c22e41fc
brendon.caulkins <brendon.caulkins@gmail.com>
2020-03-25 18:36:17 -0400

    Some small amount of Auth Guard tests

--- src/app/auth/auth.guard.spec.ts
+++ src/app/auth/auth.guard.spec.ts
@@ -1,20 +1,57 @@
-import { TestBed, inject } from '@angular/core/testing'
+import { Route } from '@angular/compiler/src/core'
+import { TestBed } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
+import { Observable } from 'rxjs'
 
+import { ErrorComponent } from '../error/error.component'
 import { MessagingModule } from '../messaging/messaging.module'
+import { Role } from './auth.enum'
 import { AuthGuard } from './auth.guard'
-import { AuthService } from './services/auth.service'
+import { AuthService, IAuthStatus } from './services/auth.service'
 import { AuthServiceFake } from './services/auth.service.fake'
 
 describe('AuthGuard', () => {
+  let authService: AuthService
+  let guard: AuthGuard
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, MessagingModule],
+      imports: [
+        RouterTestingModule.withRoutes([{ path: 'login', component: ErrorComponent }]),
+        MessagingModule,
+      ],
       providers: [AuthGuard, { provide: AuthService, useClass: AuthServiceFake }],
     })
+    authService = TestBed.inject(AuthService)
+    guard = TestBed.inject(AuthGuard)
   })
 
-  it('should be created', inject([AuthGuard], (service: AuthGuard) => {
-    expect(service).toBeTruthy()
-  }))
+  it('should be created', () => {
+    expect(guard).toBeTruthy()
+  })
+
+  describe('canLoad', () => {
+    it('should return true if user is authenticated', done => {
+      authService.authStatus$.next({
+        isAuthenticated: true,
+        userRole: Role.User,
+      } as IAuthStatus)
+      const canLoad = guard.canLoad({} as Route) as Observable<boolean>
+      canLoad.subscribe(allowed => {
+        expect(allowed).toBe(true)
+        done()
+      })
+    })
+    it('should return false if user is NOT authenticated', done => {
+      authService.authStatus$.next({
+        isAuthenticated: false,
+        userRole: Role.None,
+      } as IAuthStatus)
+      const canLoad = guard.canLoad({} as Route) as Observable<boolean>
+      canLoad.subscribe(allowed => {
+        expect(allowed).toBe(false)
+        done()
+      })
+    })
+  })
 })
