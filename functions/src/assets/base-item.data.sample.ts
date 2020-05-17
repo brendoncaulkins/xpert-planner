6ffe0e70c104215a6bd62c6d86a6f24379621169
brendon.caulkins <brendon.caulkins@gmail.com>
2020-05-17 14:54:33 -0400

    Add sample data files for functions

---
+++ functions/src/assets/base-item.data.sample.ts
@@ -0,0 +1,58 @@
+import { IBasePlanItem } from '../data.models'
+
+export const baseItems: IBasePlanItem[] = [
+  {
+    id: 1,
+    categoryId: 1,
+    type: 'Cat 1 Item 1',
+    points: 3,
+  },
+  {
+    id: 2,
+    categoryId: 1,
+    type: 'Cat 1 Item 2',
+    points: 5,
+  },
+  {
+    id: 3,
+    categoryId: 1,
+    type: 'Cat 1 Item 3',
+    points: 10,
+  },
+  {
+    id: 4,
+    categoryId: 2,
+    type: 'Cat 2 Item 1',
+    points: 3,
+  },
+  {
+    id: 5,
+    categoryId: 2,
+    type: 'Cat 2 Item 2',
+    points: 5,
+  },
+  {
+    id: 6,
+    categoryId: 2,
+    type: 'Cat 2 Item 3',
+    points: 10,
+  },
+  {
+    id: 7,
+    categoryId: 3,
+    type: 'Cat 3 Item 1',
+    points: 3,
+  },
+  {
+    id: 8,
+    categoryId: 3,
+    type: 'Cat 3 Item 2',
+    points: 5,
+  },
+  {
+    id: 9,
+    categoryId: 3,
+    type: 'Cat 3 Item 3',
+    points: 10,
+  },
+]
---
+++ functions/src/assets/category.data.sample.ts
@@ -0,0 +1,16 @@
+import { ICategory } from '../data.models'
+
+export const categories: ICategory[] = [
+  {
+    id: 1,
+    name: 'Category 1 Name',
+  },
+  {
+    id: 2,
+    name: 'Category 2 Name',
+  },
+  {
+    id: 3,
+    name: 'Category 3 Name',
+  },
+]
